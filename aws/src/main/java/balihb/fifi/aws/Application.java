75c1f7ec86361285cf60f0a7cb706cee9db76859
Hamorszky, Balazs <bhamorszky@chemaxon.com>
2019-03-01 15:31:05 +0100

    aws

---
+++ aws/src/main/java/balihb/fifi/aws/Application.java
@@ -0,0 +1,33 @@
+package balihb.fifi.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import balihb.fifi.fib.impl.FibImpl;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"balihb.fifi.fib.impl", "balihb.fifi.fib.api"})
+//@Import({ FibImpl.class })
+public class Application extends SpringBootServletInitializer {
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(Application.class).run(args);
+    }
+
+}
---
+++ aws/src/main/java/balihb/fifi/aws/StreamLambdaHandler.java
@@ -0,0 +1,18 @@
+package balihb.fifi.aws;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+}
